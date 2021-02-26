c0318f67e07ded11645803d79a4ad86a3f7ea609
brettbrosius <brettbrosius11@gmail.com>
2021-02-26 18:24:03 +0000

    committed todo list

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export function App() {
+	let list = [];
+	const [todoList, updateTodos] = useState(list);
 
-//create your first component
-export function Home() {
+	let addTask = e => {
+		if (e.keyCode == 13) {
+			let userTask = e.target.value;
+			let newList = [...todoList, userTask];
+			updateTodos(newList);
+			e.target.value = "";
+		}
+	};
+	let deleteTask = i => {
+		let updatedList = todoList.filter((element, index) => index !== i);
+		updateTodos(updatedList);
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mx-auto todoList">
+			<h1 className="">To-dos</h1>
+			<input
+				className="form-control"
+				type="text"
+				name="task"
+				placeholder="What needs to be done?"
+				onKeyDown={addTask}
+			/>
+			<ul className="list-group">
+				{todoList.map((todo, index) => (
+					<li className="list-group-item" key={index}>
+						{todo}
+						<button
+							id="xbutton"
+							className="btn btn-light"
+							onClick={() => deleteTask(index)}>
+							X
+						</button>
+					</li>
+				))}
+			</ul>
+			<p> {todoList.length} items left</p>
 		</div>
 	);
 }
+// let xbutton = document.getElementbyID("xbutton");
+// xbutton.addEventListener("mouseover", function() {
+// 	event.target.style.display = "show";
+// });
